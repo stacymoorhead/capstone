c65d7dfaea1a14445af8ae42f25c69d051980f68
Stacy Moorhead <spezzola@uarts.edu>
2017-07-25 13:02:26 +0000

    added styles for sidebar, got it displaying properly on archive and blog, updated media replace plugin

--- wp-content/themes/dyeinstyle/archive.php
+++ wp-content/themes/dyeinstyle/archive.php
@@ -11,34 +11,40 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
+			<section class="home-archive-content">
+				<div class="home-archive-posts">
+					<?php
+					if ( have_posts() ) : ?>
+			
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
+			
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_format() );
+			
+						endwhile;
+			
+						the_posts_navigation();
+			
+					else :
+			
+						get_template_part( 'template-parts/content', 'none' );
+			
+					endif; ?>
+				
+				</div><!-- .archive-content -->
+				<div class="home-archive-sidebar">
+					<?php get_sidebar(); ?>
+				</div><!-- .archive-sidebar -->
+			</section><!-- .archive-content -->	
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
